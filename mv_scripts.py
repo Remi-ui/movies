33332e669d7dd4d092353cfe610d82f649b6fab8
root <root@DESKTOP-V8IFR35.localdomain>
2021-03-17 10:59:34 +0100

    Added a filter for the dialogue of characters and made the character filter work flawlessly. Also redid the code to work with read instead of readlines

--- mv_scripts.py
+++ mv_scripts.py
@@ -20,29 +20,41 @@ def check_input(argv):
             please give only one text file as an argument to this program")
         exit(-1)
 
-def filter_character(line):
+def split_input(data):
+
+	''' Splits the input on two newline characters '''
+	data_list = re.split(r'\n\n',data)
+	return data_list
+
+def filter_character_dialogue(line):
 	
-	''' Filters on all character names in the script '''
+	''' Filters on all character names and their dialogue in the script
+		cleanlist[0] is always the character
+		cleanlist[1] is always the dialogue of that character
+	'''
 	if re.match(r'([\040]{26})([^\s])', line):
-		# Add here anything that'd need to happen with the character names
-		pass
+		char_dia = re.split(r'\s{2,}', line[26:], 1)
+		cleanlist = []
+		for item in char_dia:
+			item = re.sub(' +', ' ', item)
+			cleanlist.append(item)
 
 def filter_scene_boundary(line):
 	
-	''' Filter all scene descriptions in the script '''
-	if re.match(r'([\040]{5})([^\s])([A-Z \d\W]+$)', line):
-		#
-		# Does not perform perfectly yet, also catches scene descriptions with just capitals on a line
-		#
+	''' Filter all scene boudaries in the script '''
+	if re.match(r'([\040\n]{5})([^\s])([A-Z \d\W]+$)', line):
+		# Add here anything that'd need to happen with the scene boundaries
 		pass
 
 def main(argv):
 	fname = check_input(argv)
 	infile = open(fname, 'r')
-	data = infile.readlines()
-	for line in data:
-		filter_character(line)
-		filter_scene_boundary(line)
+	data = infile.read()
+	data_list = split_input(data)
+	for item in data_list:
+		item = item.replace('\n', '')
+		filter_character_dialogue(item)
+		filter_scene_boundary(item)
 
 
 # Making the main conditional 
