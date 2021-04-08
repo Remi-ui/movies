effbaced5eecdf6c57cc267e79f21bb798b48901
root <root@DESKTOP-V8IFR35.localdomain>
2021-04-08 15:33:10 +0200

    The range in which the subtitle will search for a match is now proportionally dependent on the difference between the subtitle list length vs the script list length

--- aligner.py
+++ aligner.py
@@ -1,5 +1,4 @@
 import sys
-from fuzzywuzzy import fuzz
 import re
 import subtitles
 import scripts
@@ -46,10 +45,15 @@ def clean_script_dialogue(script_list):
             script_dialogue.append(element)
     return script_dialogue
 
-def default_search_match(element, script_list, i):
+def default_search_match(element, script_list, i, ratio):
     best_match = 0
     best_match_script = ""
-    for y in range(15):
+    # Have the range in which the subtitle will search for a match be dependent on the ratio 
+    # between the length of the list of the script and the length of the subtitle list
+    ratio = 15 * ratio
+    if int(ratio) < 15:
+        ratio = 15
+    for y in range(int(ratio)):
         if i - y >= 0:
             # print(y, "score: ", jellyfish.jaro_winkler_similarity(element, script_list[i - y]), "subtitle: ", element, "script: " ,script_list[i - y])
             if jellyfish.jaro_winkler_similarity(element, script_list[i - y]) > best_match:
@@ -86,9 +90,9 @@ def select_dialogue(subtitle_list, script_list):
             script_list = script_list[script_list.index(best_match_script):]
             subtitle_list = subtitle_list[subtitle_list.index(el_list):]
             i = 0
-            best_match, best_match_script = default_search_match(element, script_list, i)
+            best_match, best_match_script = default_search_match(element, script_list, i, len(script_list) / len(subtitle_list))
         else: 
-            best_match, best_match_script = default_search_match(element, script_list, i)
+            best_match, best_match_script = default_search_match(element, script_list, i, len(script_list) / len(subtitle_list))
         
         print("Score: ", best_match,"Subtitle: ", element, "Script: " ,best_match_script)
 
